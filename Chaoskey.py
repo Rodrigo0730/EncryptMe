a83c068d0f0f4b02032b57d4d3dc5024d04d278a
Rodrigo0730 <rodrigo.morales@helsinki.fi>
2024-07-29 13:50:57 +0300

    finished encryption and decryption processes, added encoding checking in chaoskey.py, and finalized the process of encryption and decryption tried succesfully with a external usb containing folders with png, jpeg, pdf, docx and txt files.

--- Chaoskey.py
+++ Chaoskey.py
@@ -40,11 +40,15 @@ class PRNG:
         return random_bytes
 
     def generate_key(self, password, length):
-        salt_from_password = hashlib.sha256(password.encode()).digest()[:16]
-        hashed_password = hashlib.pbkdf2_hmac("sha256", password.encode(), salt_from_password, 100000)
+        if isinstance(password, bytes):
+            salt_from_password = hashlib.sha256(password).digest()[:16]
+            hashed_password = hashlib.pbkdf2_hmac("sha256", password, salt_from_password, 100000)
+
+        else:
+            salt_from_password = hashlib.sha256(password.encode()).digest()[:16]
+            hashed_password = hashlib.pbkdf2_hmac("sha256", password.encode(), salt_from_password, 100000)
 
         offset = int.from_bytes(hashed_password[:2], "big") / 65536.0 * 0.2 - 0.1
-        print(f"offset: {offset}")
 
         x_initial = self.safe_values[0][0] + offset
         mu = self.safe_values[0][1] + offset
@@ -62,60 +66,3 @@ if __name__ == "__main__":
     password = "pepE"
     key = prng.generate_key(password, 16)
     print(key, f"Key length: {len(key)}")
-   
-
-
-    """
-    PLOTS
-    def calculate_lyapunov_exponent(x_initial, mu, num_iterations, num_trials=1000):
-        sum_log = 0
-        for _ in range(num_trials):
-            x = x_initial
-            for _ in range(num_iterations):
-                x = sine_map(x, mu)
-                sum_log += abs(math.log(abs(sine_map_derivative(x, mu))))
-        return sum_log / (num_iterations * num_trials)
-
-    def sine_map_derivative(x, mu):
-        if x < 0.5:
-            return ((4-mu)/4) * math.pi * math.cos(math.pi * x) + mu/2
-        elif x >= 0.5:
-            return ((4-mu)/4) * math.pi * math.cos(math.pi * x) - mu/2
-
-
-    # Set the parameters
-    x_initial = 0.27
-    num_iterations = 100
-    num_mu_values = 1000
-
-    # Generate the sequence and calculate the Lyapunov exponent for each mu value
-    mu_values = np.linspace(0, 4, num_mu_values)
-    sequence = generate_sequence(x_initial, mu_values[0], num_iterations)
-    lyapunov_exponents = []
-    for mu in mu_values:
-        lyapunov_exponents.append(calculate_lyapunov_exponent(x_initial, mu, num_iterations))
-
-    # Create a list of iteration numbers for plotting
-    iteration_nums = list(range(num_iterations + 1))
-
-    # Plot the sequence
-    plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.plot(iteration_nums, sequence, 'b-')
-    plt.xlabel('Iterations')
-    plt.ylabel('X Values')
-    plt.title(f'Sine Map Sequence with mu = {mu_values[0]}')
-    plt.grid(True)
-
-    # Plot the Lyapunov exponent against mu
-    plt.subplot(1, 2, 2)
-    plt.plot(mu_values, lyapunov_exponents, 'r-')
-    plt.xlabel('Mu Values')
-    plt.ylabel('Lyapunov Exponent')
-    plt.title('Lyapunov Exponent vs. Mu')
-    plt.grid(True)
-
-    plt.tight_layout()
-    plt.show()
-
-    """
--- DeviceEncryption.py
+++ DeviceEncryption.py
@@ -1,102 +1,205 @@
-import ChaosAES as CAES
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import Chaoskey as ck
-from pathlib import Path
 import os
 import psutil
-
-def encryptFilename(aes, filename):
-    iv, encrypted_data_with_tag = aes.encryptString(filename)
-    return iv.hex() + encrypted_data_with_tag.hex()
-
-def decryptFilename(aes, encrypted_filename):
-    iv = bytes.fromhex(encrypted_filename[:24])
-    encrypted_data_with_tag = bytes.fromhex(encrypted_filename[24:])
-    return aes.decryptString(iv, encrypted_data_with_tag)
-
-def encryptDirectory(aes, inputDirectory, outputDirectory):
-    for root, dirs, files in os.walk(inputDirectory):
-        for file in files:
-            inputFilePath = os.path.join(root, file)
-            relativePath = os.path.relpath(inputFilePath, inputDirectory)
-            
-            encrypted_filename = encryptFilename(aes, relativePath)
-            outputFilePath = os.path.join(outputDirectory, encrypted_filename)
-
-            # Skip files in the output directory
-            if outputDirectory in inputFilePath:
-                continue
-
-            os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
-
-            try:
-                with open(inputFilePath, 'rb') as f:
-                    data = f.read()
-                iv, encrypted_data_with_tag = aes.encrypt(data)
-                with open(outputFilePath, 'wb') as f:
-                    f.write(iv + encrypted_data_with_tag)
-            except Exception as e:
-                print(f"Failed to encrypt {inputFilePath}: {e}")
-
-def decryptDirectory(aes, inputDirectory, outputDirectory):
-    for root, dirs, files in os.walk(inputDirectory):
-        for file in files:
-            inputFilePath = os.path.join(root, file)
-            relativePath = os.path.relpath(inputFilePath, inputDirectory)
-            
-            decrypted_filename = decryptFilename(aes, relativePath)
-            outputFilePath = os.path.join(outputDirectory, decrypted_filename)
-
-            # Skip files in the output directory
-            if outputDirectory in inputFilePath:
-                continue
-
-            os.makedirs(os.path.dirname(outputFilePath), exist_ok=True)
-
-            try:
-                with open(inputFilePath, 'rb') as f:
-                    iv = f.read(12)
-                    encrypted_data_with_tag = f.read()
-                decrypted_data = aes.decrypt(iv, encrypted_data_with_tag)
-                with open(outputFilePath, 'wb') as f:
-                    f.write(decrypted_data)
-            except Exception as e:
-                print(f"Failed to decrypt {inputFilePath}: {e}")
+import hashlib
 
 
+class AES:
+    def __init__(self, password, key_file=None):
+ 
+        self.check_password_requirements(password)
+        if isinstance(password, str):
+            self.password = password.encode('utf-8')
+        else:
+            self.password = password
+        self.key = None     
 
-def recognizeDrives():
+        if key_file:
+            self.load_key_from_file(key_file)
+        else:
+            self.key = self._derive_aes_key()
+
+                  
+    def _derive_aes_key(self):
+        try:
+            # Generate a random 256-bit key using PBKDF2HMAC with a PRNG generated salt
+            prng = ck.PRNG()
+            salt = prng.generate_key(self.password, 16)
+            kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=salt,
+                iterations=1000000,
+                backend=default_backend()
+            )
+            return kdf.derive(self.password)
+        except Exception as e:
+            raise ValueError("Error deriving AES key: {}".format(e))
+
+    def _get_aes_key(self):
+        if self.key is None:
+            self.key = self._derive_aes_key()
+        return self.key
+
+    def save_key_to_file(self, key_file):
+        try:
+            with open(key_file, "wb") as f:
+                key = self._get_aes_key()
+                f.write(key)
+        except Exception as e:
+            raise ValueError("Error saving key to file: {}".format(e))
+
+    def load_key_from_file(self, key_file):
+        try:
+            with open(key_file, "rb") as f:
+                self.key = f.read()
+        except Exception as e:
+            raise ValueError("Error loading key from file: {}".format(e))
+
+
+    def encrypt(self, data):
+        prng = ck.PRNG()
+        iv = prng.generate_key(self.password, 12)
+        key = self._get_aes_key()
+        cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend())
+        encryptor = cipher.encryptor()
+        encrypted_data = encryptor.update(data) + encryptor.finalize()
+        encrypted_data_with_tag = encrypted_data + encryptor.tag
+        return iv, encrypted_data_with_tag
+    
+    def decrypt(self, iv, encrypted_data_with_tag):
+        encrypted_data = encrypted_data_with_tag[:-16]
+        tag = encrypted_data_with_tag[-16:]
+        cipher = Cipher(algorithms.AES(self._get_aes_key()), modes.GCM(iv, tag), backend=default_backend())
+        decryptor = cipher.decryptor()
+        decrypted_data = decryptor.update(encrypted_data) + decryptor.finalize()
+        return decrypted_data
+    
+    @staticmethod
+    def check_password_requirements(password):
+        min_length = 8
+        requires_uppercase = True
+        requires_lowercase = True
+        requires_digit = True
+        requires_special_char = True
+
+        conditions_not_met = []
+
+        if len(password) < min_length:
+            conditions_not_met.append(f"Password should be at least {min_length} characters long.")
+
+        if requires_uppercase and not any(char.isupper() for char in password):
+            conditions_not_met.append("Password should contain at least one uppercase character.")
+
+        if requires_lowercase and not any(char.islower() for char in password):
+            conditions_not_met.append("Password should contain at least one lowercase character.")
+
+        if requires_digit and not any(char.isdigit() for char in password):
+            conditions_not_met.append("Password should contain at least one digit.")
+
+        special_chars = r"!@#$%^&*()_-+={}[]|\:;\"'<>,.?/~"
+        if requires_special_char and not any(char in special_chars for char in password):
+            conditions_not_met.append("Password should contain at least one special character.")
+
+        if conditions_not_met:
+            if len(password) > 0:
+                print("Password requirements not met:")
+                for condition in conditions_not_met:
+                    print(condition)
+            raise ValueError("Try again")
+
+        return
+
+def recognize_drives():
     drives = []
     for drive in psutil.disk_partitions():
         if 'removable' in drive.opts:
             drives.append(drive.device)
     return drives
 
-def main():
-    drives = recognizeDrives()
-    if not drives:
-        print("no removable drives")
+def encryptDirectory(password):
+    try:
+        aes = AES(password)
+        device = recognize_drives()[0]
+        outputFilePath = os.path.join(device, 'encrypted_data.aes')
+        data = {}
+
+        for root, dirs, files in os.walk(device):
+            for file in files:
+                inputFilePath = os.path.join(root, file)
+                relativePath = os.path.relpath(inputFilePath, device)
+                
+                with open(inputFilePath, 'rb') as f:
+                    data[relativePath] = f.read()
+
+        all_data = '\n'.join(f"{path}:{content.hex()}" for path, content in data.items()).encode()
+        hashed_data = hashlib.sha256(all_data).hexdigest()
+        data_with_hash = f"{hashed_data}\n".encode() + all_data
+
+        iv, encrypted_data_with_tag = aes.encrypt(data_with_hash)
+
+        with open(outputFilePath, 'wb') as f:
+            f.write(iv + encrypted_data_with_tag)
+            f.flush()
+            os.fsync(f.fileno())
+
+        for root, _, files in os.walk(device):
+            for file in files:
+                inputFilePath = os.path.join(root, file)
+                if inputFilePath == outputFilePath:
+                    continue
+                else:
+                    os.remove(inputFilePath)
+        for root, dirs, _ in os.walk(device, topdown=False):
+            for dir in dirs:
+                os.rmdir(os.path.join(root, dir))
+    except ValueError as e:
+        print(e)
+
+def decryptDirectory(password):
+    
+    aes = AES(password)
+    device = recognize_drives()[0]
+    inputFilePath = os.path.join(device, 'encrypted_data.aes')  
+    outputDirectory = device
+
+    with open(inputFilePath, 'rb') as f:
+        iv = f.read(12)
+        encrypted_data_with_tag = f.read()
+
+    try:
+        data_with_hash = aes.decrypt(iv, encrypted_data_with_tag)
+    except Exception as e:
+        print(f"Password is incorrect")
         return
     
-    selected_drive = drives[0]   
-    print(f"Found removable drive: {selected_drive}") 
+    stored_hash, data = data_with_hash.decode().split('\n', 1)
+    hash = hashlib.sha256(data.encode()).hexdigest()
 
-    passphrase = "my_secure_passphrase"
+    if stored_hash != hash:
+        print("Hash mismatch")
+        return
+    
+    for entry in data.split('\n'):
+        relativePath, content = entry.split(':', 1)
+        outputFilePath = os.path.join(outputDirectory, relativePath)
 
-    # Create an AES object
-    aes = CAES.AES(passphrase)
+        os.makedirs(os.path.dirname(outputFilePath), exist_ok = True)
+        with open(outputFilePath, 'wb') as f:
+            f.write(bytes.fromhex(content))
 
-    # Define the directories
-    input_dir = selected_drive
-    encrypted_dir = os.path.join(selected_drive, "encrypted")    
-    decrypted_dir = os.path.join(selected_drive, "decrypted")
+    print("Decryption completed successfully")
 
-    # Encrypt the directory
-    encryptDirectory(aes, input_dir, encrypted_dir)
-    print("Encryption complete.")
+    os.remove(inputFilePath)
 
-    # Decrypt the directory
-    decryptDirectory(aes, encrypted_dir, decrypted_dir)
-    print("Decryption complete.")
+
+
+def main():
+    decryptDirectory("Password1:")
 
 if __name__ == "__main__":
     main()
