18601ced88087cccc16c9d3dbe5d0ddd12e917c6
Rodrigo0730 <rodrigo.morales@helsinki.fi>
2024-08-14 00:26:33 +0300

    updated progress_callback for progress bar on main

--- DeviceEncryption.py
+++ DeviceEncryption.py
@@ -1,53 +1,52 @@
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-import chaosrandom
-import os
-import psutil
 import hashlib
-import zlib
+import os
 import struct
+import zlib
+
+import psutil
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+import chaosrandom
 
 
 class AES:
     def __init__(self, password, salt=None):
-        self.check_password_requirements(password)
-        self.password = password.encode("utf-8") if isinstance(password, str) else password
-
         self.key = None
         self.salt = salt
+        self.password = (
+            password.encode("utf-8") if isinstance(password, str) else password
+        )
 
-        self.key, self.salt = self._derive_key_and_salt()
-
-    def _derive_key_and_salt(self):
         if self.salt is None:
-            prng = chaosrandom.PRNG()
-            salt = prng.generate_key(16)
-        else:
-            salt = self.salt
+            self.check_password_requirements(password)
+            self.salt = self._generate_salt()
 
+        self.key = self._derive_key()
+
+    def _generate_salt(self):
+        prng = chaosrandom.PRNG()
+        return prng.generate_key(16)
+
+    def _derive_key(self):
         try:
             kdf = PBKDF2HMAC(
                 algorithm=hashes.SHA256(),
                 length=32,
-                salt=salt,
+                salt=self.salt,
                 iterations=1000000,
                 backend=default_backend(),
             )
-            return kdf.derive(self.password), salt
+            return kdf.derive(self.password)
         except Exception as e:
             raise ValueError("Error deriving AES key: {}".format(e))
-
-    def _get_aes_key(self):
-        if self.key is None:
-            self.key, self.salt = self._derive_aes_key()
-        return self.key, self.salt
 
     def encrypt(self, data):
         prng = chaosrandom.PRNG()
         iv = prng.generate_key(12)
-        key, _ = self._get_aes_key()
+        key = self.key
         cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend())
         encryptor = cipher.encryptor()
         encrypted_data = encryptor.update(data) + encryptor.finalize()
@@ -58,7 +57,7 @@ class AES:
         iv = encrypted_data_with_tag[:12]
         tag = encrypted_data_with_tag[12:28]
         encrypted_data = encrypted_data_with_tag[28:]
-        key, _ = self._get_aes_key()
+        key = self.key
         cipher = Cipher(
             algorithms.AES(key),
             modes.GCM(iv, tag),
@@ -126,78 +125,79 @@ def encryptDirectory(directory, password, progress_callback=None):
     try:
         aes = AES(password, salt=None)
         salt = aes.salt
-        device = directory
-        outputFilePath = os.path.join(device, "encrypted_data.aes")
-        data = b""
+        output_file_path = os.path.join(directory, "encrypted_data.aes")
+        data = bytearray()
+        files_to_delete, total_files = [], 0
 
-        total_files = sum(len(files) for _, _, files in os.walk(device))
-        processed_files = 0
-
-        for root, dirs, files in os.walk(device):
+        for root, dirs, files in os.walk(directory):
             for file in files:
-                inputFilePath = os.path.join(root, file)
-                relativePath = os.path.relpath(inputFilePath, device)
+                file_path = os.path.join(root, file)
+                if (
+                    "System Volume Information"
+                    not in os.path.relpath(file_path, directory)
+                    and file_path != output_file_path
+                ):
+                    files_to_delete.append(file_path)
+                    total_files += 1
 
-                with open(inputFilePath, "rb") as f:
-                    file_data = f.read()
+        processed_files = 0
+        for file_path in files_to_delete:
+            relative_path = os.path.relpath(file_path, directory)
 
-                if "System Volume Information" in relativePath or file.startswith('.'):
-                    continue
+            with open(file_path, "rb") as f:
+                file_data = f.read()
 
-                compressed_data = zlib.compress(file_data)
-                path_length = len(relativePath.encode("utf-8"))
-                data += struct.pack(f'<I{path_length}sI', path_length, relativePath.encode(), len(compressed_data))
-                data += compressed_data
+            compressed_data = zlib.compress(file_data)
+            path_length = len(relative_path.encode("utf-8"))
+            data += struct.pack(
+                f"<I{path_length}sI",
+                path_length,
+                relative_path.encode(),
+                len(compressed_data),
+            )
+            data += compressed_data
 
-                processed_files += 1
-                if progress_callback:
-                    progress = (processed_files / total_files) * 100
-                    progress_callback(progress)
+            os.remove(file_path)
+
+            processed_files += 1
+            if progress_callback:
+                progress_callback(processed_files, total_files)
 
         hashed_data = hashlib.sha256(data).digest()
         data_with_hash = hashed_data + data
 
         encrypted_data_with_tag = aes.encrypt(data_with_hash)
 
-        with open(outputFilePath, "wb") as f:
+        with open(output_file_path, "wb") as f:
             f.write(salt + encrypted_data_with_tag)
-            f.flush()
-            os.fsync(f.fileno())
 
-        for root, _, files in os.walk(device):
-            for file in files:
-                inputFilePath = os.path.join(root, file)
-                if inputFilePath == outputFilePath or "System Volume Information" in inputFilePath or file.startswith('.'):
-                    continue
-                else:
-                    os.remove(inputFilePath)
-        for root, dirs, _ in os.walk(device, topdown=False):
-            for dir in dirs:
-                if dir == "System Volume Information" or dir.startswith('.'):
-                    continue
-                os.rmdir(os.path.join(root, dir))
+        dirs_to_delete = [
+            os.path.join(root, dir)
+            for root, dirs, files in os.walk(directory)
+            for dir in dirs
+            if "System Volume Information" not in dir
+        ]
+
+        for dir_path in reversed(dirs_to_delete):
+            os.rmdir(dir_path)
+
     except ValueError as e:
-        print(e)
+        raise ValueError(f"Error: {e}")
 
 
-def decryptDirectory(directory, password):
-    device = directory
-    inputFilePath = os.path.join(device, "encrypted_data.aes")
-    outputDirectory = device
+def decryptDirectory(directory, password, progress_callback=None):
+    inputFilePath = os.path.join(directory, "encrypted_data.aes")
+    outputDirectory = directory
 
     with open(inputFilePath, "rb") as f:
         salt = f.read(16)
-        print(salt, type(salt))
         encrypted_data_with_tag = f.read()
-
     aes = AES(password, salt=salt)
-    print(aes.salt == salt)
 
     try:
         data_with_hash = aes.decrypt(encrypted_data_with_tag)
     except Exception:
-        print("Password is incorrect")
-        return
+        raise ValueError("Password is incorrect")
 
     stored_hash = data_with_hash[:32]
     data = data_with_hash[32:]
@@ -209,14 +209,24 @@ def decryptDirectory(directory, password):
 
     index = 0
     while index < len(data):
-        path_length = struct.unpack("<I",data[index:index+4])[0]
+        if progress_callback:
+            progress_callback(index, len(data))
+        path_length = struct.unpack("<I", data[index : index + 4])[0]
         index += 4
-        relativePath = data[index:index+path_length].decode()
+        if progress_callback:
+            progress_callback(index, len(data))
+        relativePath = data[index : index + path_length].decode()
         index += path_length
-        data_length = struct.unpack("<I", data[index:index+4])[0]
+        if progress_callback:
+            progress_callback(index, len(data))
+        data_length = struct.unpack("<I", data[index : index + 4])[0]
         index += 4
-        compressed_data = data[index:index+data_length]
+        if progress_callback:
+            progress_callback(index, len(data))
+        compressed_data = data[index : index + data_length]
         index += data_length
+        if progress_callback:
+            progress_callback(index, len(data))
         file_data = zlib.decompress(compressed_data)
 
         outputFilePath = os.path.join(outputDirectory, relativePath)
@@ -226,13 +236,12 @@ def decryptDirectory(directory, password):
         with open(outputFilePath, "wb") as f:
             f.write(file_data)
 
-    print("Decryption completed successfully")
-
     os.remove(inputFilePath)
+
 
 def main():
     directory = recognize_drives()[0]
-    encryptDirectory(directory=directory, password="Password1:")
+    # encryptDirectory(directory=directory, password="Password1:")
     decryptDirectory(directory=directory, password="Password1:")
 
 
